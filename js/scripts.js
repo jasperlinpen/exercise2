a2866bbf59f30b40533fbce6bc27df598fc0d333
anthonylinwen <79689319+anthonylinwen@users.noreply.github.com>
2023-11-12 17:33:46 +0800

    www
    www

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,17 @@
           const element1 = document.getElementById("myBar1");
-          let width = 0 , intervalIds = [] ; 
+          let width = 0 , intervalIds = []  ;
+		  let str_1="https://ws.api.cnyes.com/ws/api/v1/charting/history?resolution=1&symbol=TWS:", 
+			  str_2="0", 
+			  str_3=":STOCK&quote=1" 
+			  ajaxURL= str_1 + str_2 + str_3; 				  
           window.addEventListener('load',function(){
-          	if (firstVisit === true) {
-          		// firstVisit = false;
-				 getDATA();
-                 getWDATA();
-          		 element1.style.width = '0%'; 
-               document.getElementById("s01").addEventListener("change", myFunction);         		
-          	}  else {	
-               getDATA();
-               getWDATA();
-			   /*
-               id = setInterval(getDATA, refSec);
-			   */
-          	}
-
+		  getDATA();
+          getWDATA();
+          element1.style.width = '0%'; 	
+          document.getElementById("s01").addEventListener("change", refreshTime); 	  
           });
-                            
-          function myFunction() {
+		                              
+          function refreshTime() {
              switch ( $(this).val()) {
                       case "0": 
                           width = 100;
@@ -25,96 +19,42 @@
                        	  element1.style.width = '0%'; 
                           break;
                       case "1": 
-                           if (firstVisit === true) {
-                      	      refSec = 3000 ;
-                              } else {
-                              firstVisit = false;
-                            //  url='index1.htm' ;
-                      	      }
+                      	  refSec = 3000 ;
                            break;
                       case "2":
-                           if (firstVisit === true) {
-                      	      refSec = 5000 ;
-                              } else {
-                              firstVisit = false;
-                            //  url='index2.htm' ;
-                      	      }
+                           refSec = 5000 ;
                            break;
                       case "3": 
-                           if (firstVisit === true) {
-                      	      refSec = 10000 ;
-                              } else {
-                              firstVisit = false;
-                            //  url='index3.htm' ;
-                      	      }
+                           refSec = 10000 ;
                            break;
                       case "4": 
-                           if (firstVisit === true) {
-                      	      refSec = 20000 ;
-                              } else {
-                              firstVisit = false;
-                            //  url='index4.htm' ;
-                      	      }
+                      	   refSec = 20000 ;
                            break;
                       case "5": 
-                           if (firstVisit === true) {
-                      	      refSec = 30000 ;
-                              } else {
-                              firstVisit = false;
-                            //  url='index5.htm' ;
-                      	      }
+                      	   refSec = 30000 ;
                            break;
                       case "6": 
-                           if (firstVisit === true) {
-                      	      refSec = 60000 ;
-                              } else {
-                              firstVisit = false;	
-                            //  url='index6.htm' ;
-                      	      }
+                    	   refSec = 60000 ;
                            break;
                       case "7": 
-                           if (firstVisit === true) {
-                      	      refSec = 600000 ;
-                              } else {
-                              firstVisit = false;
-                            //  url='index7.htm' ;
-                      	      }
+                   	      refSec = 600000 ;
                            break; 
                       case "8": 
-                           if (firstVisit === true) {
-                      	      refSec = 900000 ;
-                              } else {
-                              firstVisit = false;
-                            //  url='index8.htm' ;
-                      	      }
+                          refSec = 900000 ;
                            break;
                       case "9": 
-                           if (firstVisit === true) {
-                      	      refSec = 1200000 ;
-                              } else {
-                              firstVisit = false;
-                            //  url='index9.htm' ;
-                      	      }
+                    	   refSec = 1200000 ;
                            break;
                       case "10": 
-                           if (firstVisit === true) {
-                      	      refSec = 1800000 ;
-                              } else {
-                              firstVisit = false;
-                            //  url='index10.htm' ;
-                      	      }
-                           break;                                     
+                   	      refSec = 1800000 ;
+                          break;                                     
                       default:
                          return;
                     } 
-
-          	 //  console.log(refSec);
-          	   if ( firstVisit === true ) {
-          	   	 //  console.log(firstVisit);
+                   str_2=document.getElementById("s02").value ;
 				   while(intervalIds.length){
                           clearInterval(intervalIds.pop());
                     }
-          	   	 //  id = setInterval(getDATA,refSec);
 				   if  (refSec != 99999 ) {
 				       id = setInterval(getDATA,refSec);
 				       intervalIds.push(id); 
@@ -126,36 +66,20 @@
                         }
 						
 					 }
-				   /*
- 				   if (id > 1) {
-					   clearInterval(id-1) ;
-					   width = 0; 
-				    }
-				    */	
-          	    }  
-				 else {
-          	     //  console.log(firstVisit);
-          	   	 //  location.replace(url) ;
-          	      }
+
             }
                      
           function getDATA() {
            	   var d = new Date();
                $('#date1').html((d.getMonth()+1) + '/' + d.getDate() + '&nbsp;' + d.getHours() + ':'  + d.getMinutes());
                if (width === 100 ) {
-                 //  clearInterval(id);
 				   width = 0; 
                    } else {
                     width += 7 ;
                    if (width > 95) width = width-95 ; 
                       element1.style.width = width + '%'; 
                    } 
-				   
-				/*  
-                if (firstVisit === false) {
-                   document.getElementById("s01").addEventListener("change", myFunction);   
-                }  
-                */             	
+				           	
                 $.getJSON('https://ws.api.cnyes.com/ws/api/v1/charting/history?resolution=1&symbol=TWS:2449:STOCK&quote=1',function(data){
                     // console.log('success');
                   $.each(data,function(key1,item1){
@@ -478,7 +402,75 @@
                   }
                  });
                 });    
-              //  Ending Weighed index section     
+              //  Ending Weighed index section   
+              //  Option selected index  section
+			 if (str_2 !="0") {
+				 ajaxURL=str_1 + str_2 + str_3 ;
+              //  console.log(ajaxURL);		 
+                 $.getJSON(ajaxURL,function(data){
+                    // console.log('success');
+                  $.each(data,function(key11,item11){
+                     if (key11 === 'data') {
+                   	//  $('ul').append('<li>'+item1+'</li>');                   	
+                    var itemData11 = item11; 	          
+                    $.each(itemData11,function(key21,item21){
+                    	if (key21  === 'quote' ) {
+                    		  var itemData21 = item21;
+                    		 // console.log(itemData21); 	
+                    		  $.each(itemData21,function(key31,item31){  
+                             if (key31 === '200009') {
+                 	              $("#op11").html(item31); 
+                             }
+                             if (key31 === '6') {
+                 	              $("#op12").html(item31); 
+                             }
+                             if (key31 === '11') {
+                             	  if (item31> 0) 
+                             	      {
+                             	       	$("#op12").addClass("risePrice"); 
+                             	       	$("#op13").addClass("risePrice"); 
+                             	      } 
+                             	  else {
+                             	  	 if (item31 === 0){ 
+                             	  	 	$("#op12").addClass("flatPrice"); 
+                             	        $("#op13").addClass("flatPrice"); 		
+                             	  	 }
+                             	  	 else {
+                             	  	  $("#op12").addClass("fellPrice"); 
+                             	       $("#op13").addClass("fellPrice"); 	
+                             	  	 }
+                             	  }
+                             	  $("#op13").html(item31); 
+                             } 
+                             if (key31 === '12') {
+                 	               $("#op14").html(item31); 
+                             }
+                             if (key31 === '13') {
+                 	               $("#op15").html(item31); 
+                             } 
+                        }) ;                 		
+                    	}
+                     });
+                   //  console.log(item1[0]);
+                       if ($("#op14").html() >= $("#op12").html() - $("#op13").html())
+                          {
+                       	    $("#op14").addClass("highestPrice");
+                          }  
+                       else {
+                       	  $("#op14").addClass("lowestPrice");
+                          }
+                       if ($("#op15").html() >= $("#op12").html() - $("#op13").html())
+                          {
+                       	  $("op15").addClass("highestPrice");
+                          }  
+                       else {
+                       	  $("#op15").addClass("lowestPrice");
+                       }                  
+                  }
+                 });
+                });  					
+				}			
+              //  Ending Option selected index section  
                };  
                
                    
